acaf0e4c9dc1d3bd66b2494b9c41ab119d06a65c
aliismaa <116195263+aliismaa@users.noreply.github.com>
2023-02-19 17:37:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,25 @@ def find_mismatch(text):
             opening_brackets_stack.append((i, next))
             
         if next in ")]}":
-            if (len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[len(opening_brackets_stack)-1].char, next) != True):
+            if len(opening_brackets_stack) == 0:
                 return i+1
             opening_brackets_stack.pop()
-    if (len(opening_brackets_stack)!=0):
-        return opening_brackets_stack[len(opening_brackets_stack)-1].position+1
+            for j in range(len(opening_brackets_stack) - 1, -1, -1):
+                if opening_brackets_stack[j][1] in "([{":
+                    if are_matching(opening_brackets_stack[j][1], next):
+                        opening_brackets_stack.pop(j)
+                        break
+                    else:
+                        return i + 1
+
+    if len(opening_brackets_stack) == 1:
+      return opening_brackets_stack[0][0] + 1
+    # success
     return 0
 
 def main():
     text = ""
-    choice = input()
-    if (choice=="F"):
-        text = open("test\\"+str(input()),"r").read()
-    else:
-        text = input()
+    text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     if (mismatch == 0):
