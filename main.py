e60a63d5c828ea22648383bf00ccb1940d09bffd
aliismaa <116195263+aliismaa@users.noreply.github.com>
2023-02-19 16:53:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,32 +15,26 @@ def find_mismatch(text):
             opening_brackets_stack.append((i, next))
             
         if next in ")]}":
-            if len(opening_brackets_stack) == 0:
-                return i + 1
-           for j in range(len(opening_brackets_stack) - 1, -1, -1):
-                if opening_brackets_stack[j][1] in "([{":
-                    if are_matching(opening_brackets_stack[j][1], next):
-                        opening_brackets_stack.pop(j)
-                        break
-                    else:
-                        return i + 1
-                
-    # if there is only one item, the error is definitely at position one
-    if len(opening_brackets_stack) == 1:
-        return opening_brackets_stack[0][0] + 1
-    # success
+            if (len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[len(opening_brackets_stack)-1].char, next) != True):
+                return i+1
+            opening_brackets_stack.pop()
+    if (len(opening_brackets_stack)!=0):
+        return opening_brackets_stack[len(opening_brackets_stack)-1].position+1
     return 0
 
 def main():
-    text = input()
+    text = ""
+    choice = input()
+    if (choice=="F"):
+        text = open("test\\"+str(input()),"r").read()
+    else:
+        text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    
-    if mismatch != 0
-        print (mismatch)
-        return 0
-    print("Success")
-    return 0
+    if (mismatch == 0):
+        print("Success")
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
