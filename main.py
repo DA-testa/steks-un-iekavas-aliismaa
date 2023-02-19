db95550883c9e5175aa973845e6992cbd29f9c97
aliismaa <116195263+aliismaa@users.noreply.github.com>
2023-02-19 17:48:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def find_mismatch(text):
         if next in ")]}":
             if len(opening_brackets_stack) == 0:
                 return i+1
-            opening_brackets_stack.pop()
             for j in range(len(opening_brackets_stack) - 1, -1, -1):
                 if opening_brackets_stack[j][1] in "([{":
                     if are_matching(opening_brackets_stack[j][1], next):
@@ -33,7 +32,11 @@ def find_mismatch(text):
 
 def main():
     text = ""
-    text = input()
+    choice = input()
+    if (choice=="F"):
+        text = open(str(input()),"r").read()
+    else:
+        text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     if (mismatch == 0):
